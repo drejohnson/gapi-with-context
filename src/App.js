1a0c75fba292a610dedee7d7996b9a44b3fca2ff
DeAndre Johnson <dre@phreshr.com>
2020-03-01 05:26:12 +0800

    add correct routes, clean up unused imports

--- src/App.js
+++ src/App.js
@@ -1,46 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
-import { Stack, Flex, Box, Grid } from '@chakra-ui/core';
+import { Stack } from '@chakra-ui/core';
 import Authenticate from './routes/Authenticate';
-import Events from './routes/Events';
-import useGapi from './hooks/useGapi';
-import { useAuth } from './contexts/auth'
-import EventService from './lib/calendarService';
+import { useAuth } from './contexts/auth';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
+import Welcome from './routes/Welcome';
+import Dashboard from './routes/Dashboard';
 
 function App() {
-  const [eventService, setEventService] = useState();
+  const { googleApi } = useAuth();
 
-  const {googleApi, api} = useAuth()
-
-  const {
-    isLoading,
-    isAuthenticated,
-    currentUser,
-    onSignIn,
-    onSignOut
-  } = googleApi;
-
-  if (isLoading) {
+  if (googleApi.isLoading) {
     return <div>Initializing...</div>;
-  }
-
-  if (!isAuthenticated) {
-    return <button onClick={onSignIn}>Signin with Google</button>;
   }
 
   return (
     <Stack pos="relative" w="100%" minHeight="100vh">
-      <h2>Email: {currentUser && currentUser.email}</h2>
-      <h2>Google ID: {currentUser && currentUser.googleId}</h2>
-      <button onClick={onSignOut}>Sign out</button>
+      <Header />
 
       <Route path="/authenticate/google">
         <Authenticate />
       </Route>
-      <Route path="/events">
-        <Events api={api} />
+      <Route exact path="/">
+        <Welcome />
       </Route>
+      <PrivateRoute path="/:id/dashboard">
+        <Dashboard />
+      </PrivateRoute>
     </Stack>
   );
 }
