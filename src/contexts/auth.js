ce6280fb75655eafb3584adb79f60a8712157d9c
DeAndre Johnson <dre@phreshr.com>
2020-03-01 04:53:57 +0800

    clean up

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -7,20 +7,18 @@ const AuthContext = React.createContext();
 function AuthProvider({ children }) {
   const [api, setApi] = React.useState(null);
 
-  const onGapiLoaded = useCallback(gapiClient =>
-    setApi(calendarApi(gapiClient))
-  );
-
   const googleApi = useGapi({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
     clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
     scope: 'https://www.googleapis.com/auth/calendar.events',
     discoveryDocs: [
       'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'
     ],
     ux_mode: 'redirect',
-    redirect_uri: 'http://localhost:3000/authenticate/google',
-    onLoaded: onGapiLoaded
+    redirect_uri: `${process.env.REACT_APP_BASE_URL}/authenticate/google`,
+    onLoaded: useCallback(gapi => {
+      setApi(calendarApi(gapi));
+    }, [])
   });
 
   return (
