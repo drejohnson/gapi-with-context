f2c197841629009493db5f623fd7b8df5207e0f6
DeAndre Johnson <dre@phreshr.com>
2020-03-02 08:23:27 +0800

    update dashboard to display calendar

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import { Box } from '@chakra-ui/core';
 import { useAuth } from '../contexts/auth';
-import Events from '../components/Events';
+import Calendar from '../components/Calendar';
 
 const Dashboard = () => {
   const { googleApi, api } = useAuth();
   const { currentUser, handleSignOut } = googleApi;
   return (
-    <div>
+    <Box
+      pos="relative"
+      backgroundColor="brand.lightgray"
+      p={[4, 16]}
+      minHeight="100vh"
+    >
       <h1>Dashboard</h1>
       <h2>Email: {currentUser.email}</h2>
       <h2>Google ID: {currentUser.googleId}</h2>
       <button onClick={handleSignOut}>Sign out</button>
-      <Events api={api} />
-    </div>
+      <Calendar api={api} />
+    </Box>
   );
 };
 
